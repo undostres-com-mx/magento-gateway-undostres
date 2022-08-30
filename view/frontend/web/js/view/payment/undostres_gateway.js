2053fe0f4b412bcd92b05614d140556242267873
CarlosAMS123 <carlos.miranda@undostres.com.mx>
2022-08-29 20:28:22 -0500

    Revert "Name Convencion"
    This reverts commit 13c9ca53bedc9edda363fe2d18c5af94d5af3f47.

--- view/frontend/web/js/view/payment/undostres_gateway.js
+++ view/frontend/web/js/view/payment/undostres_gateway.js
@@ -11,8 +11,8 @@ define(
         'use strict';
         rendererList.push(
             {
-                type: 'undostres_gateway',
-                component: 'magento_gateway_undostres/js/view/payment/method-renderer/undostres_gateway'
+                type: 'undostres-gateway',
+                component: 'magento-gateway-undostres/js/view/payment/method-renderer/undostres-gateway'
             }
         );
         return Component.extend({});
