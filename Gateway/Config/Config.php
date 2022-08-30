c546f43d14c652459f65571ee0dd5b32fd125359
CarlosAMS123 <carlos.miranda@undostres.com.mx>
2022-08-30 16:29:26 -0500

    Update Config.php

--- Gateway/Config/Config.php
+++ Gateway/Config/Config.php
@@ -9,7 +9,7 @@ use \Magento\Payment\Gateway\Config\Config as MagentoConfig;
 class Config extends MagentoConfig
 {
     /* KEYS INSIDE MAGENTO CONFIG */
-    const CODE = 'Undostres_Gateway';
+    const CODE = 'UDT_Gateway';
     const KEY_TITLE = 'title';
     const KEY_DESCRIPTION = 'description';
     const KEY_GATEWAY_LOGO = 'gateway_logo';
