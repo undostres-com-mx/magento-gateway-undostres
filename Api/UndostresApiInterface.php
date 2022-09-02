3723e71be01656e105cab9a0a6ae9b383462c7b2
CarlosAMS123 <carlos.miranda@undostres.com.mx>
2022-09-02 14:17:26 -0500

    Update UndostresApiInterface.php

--- Api/UndostresApiInterface.php
+++ Api/UndostresApiInterface.php
@@ -2,8 +2,21 @@
 
 namespace Undostres\PaymentGateway\Api;
 
+/**
+ * @api
+ */
 interface UndostresApiInterface
 {
+    /**
+     * @param $paymentId
+     * @param $status
+     * @return mixed
+     */
     public function callback($paymentId, $status);
+
+    /**
+     * @param $orderId
+     * @return mixed
+     */
     public function redirect($orderId);
 }
