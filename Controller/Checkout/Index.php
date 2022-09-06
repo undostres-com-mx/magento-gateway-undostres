56f55b79bd99fd4ff3976fe9b3ecd077974964f7
CarlosAMS123 <carlos.miranda@undostres.com.mx>
2022-09-06 18:43:01 -0500

    Update Index.php

--- Controller/Checkout/Index.php
+++ Controller/Checkout/Index.php
@@ -3,6 +3,7 @@
 namespace Undostres\PaymentGateway\Controller\Checkout;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Undostres\PaymentGateway\Helper\Helper;
 
 /* ACTION WHERE THE ORDER IS PASSED TO UDT GATEWAY AND REDIRECTION IS DONE */
@@ -11,19 +12,19 @@ class Index extends Action
 {
     protected $helper;
 
-    public function __construct(Helper $helper)
+    public function __construct(Context $context, Helper $helper)
     {
-        parent::__construct();
+        parent::__construct(context);
         $this->helper = $helper;
     }
 
     /* HANDLE ERROR AND REDIRECT */
     private function throwError($order, $restoreCart)
     {
-       /* $this->addFrontMesage(Helper::MSG_WARNING, 'Su orden no fue procesada correctamente, favor de recargar la pagina');
-        if ($restoreCart === true) $this->restoreCart();
-        $this->cancelOrder($order);
-        $this->redirectToCheckoutCart();*/
+        /* $this->addFrontMesage(Helper::MSG_WARNING, 'Su orden no fue procesada correctamente, favor de recargar la pagina');
+         if ($restoreCart === true) $this->restoreCart();
+         $this->cancelOrder($order);
+         $this->redirectToCheckoutCart();*/
     }
 
 
@@ -31,20 +32,20 @@ class Index extends Action
     {
         $this->helper->log("Entro al index :D ");
         //$order = $this->getOrder();
-       /* try {
-            if ($order === null) $this->throwError($order, false);
-            else {
-                if ($this->isOrderPending($order)) {
-                    $json = $this->getOrderJSON($order);
-                    $gatewayUrl = $this->getPaymentUrl($json);
-                    if ($gatewayUrl === null) $this->throwError($order, true);
-                    else $this->redirectPage($gatewayUrl);
-                } else $this->throwError($order, false);
-            }
-        } catch (\Exception $ex) {
-            $this->log('Ocurrió una excepción con la orden Undostres/checkout/index: ' . $ex->getMessage());
-            $this->log($ex->getTraceAsString());
-            $this->throwError($order, true);
-        }*/
+        /* try {
+             if ($order === null) $this->throwError($order, false);
+             else {
+                 if ($this->isOrderPending($order)) {
+                     $json = $this->getOrderJSON($order);
+                     $gatewayUrl = $this->getPaymentUrl($json);
+                     if ($gatewayUrl === null) $this->throwError($order, true);
+                     else $this->redirectPage($gatewayUrl);
+                 } else $this->throwError($order, false);
+             }
+         } catch (\Exception $ex) {
+             $this->log('Ocurrió una excepción con la orden Undostres/checkout/index: ' . $ex->getMessage());
+             $this->log($ex->getTraceAsString());
+             $this->throwError($order, true);
+         }*/
     }
 }
