37953a062f94399866d8e3302bbeb4c57ab4a5af
CarlosAMS123 <carlos.miranda@undostres.com.mx>
2022-09-05 21:04:30 -0500

    Update Config.php

--- Model/Config.php
+++ Model/Config.php
@@ -2,36 +2,55 @@
 
 namespace Undostres\PaymentGateway\Model;
 
-use Magento\Payment\Gateway\Config\Config as MagentoConfig;
+//use Magento\Payment\Gateway\Config\Config as MagentoConfig;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 /* EXTRACTS INFO FROM MAGENTO CONFIG THROUGH CONFIG EXTENDED CLASS - DICTIONARY */
 
-class Config extends MagentoConfig
+class Config
 {
-    const CODE = 'Undostres_Gateway';
+    const CODE = "Undostres_Gateway";
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    public function __construct(Context $context, ScopeConfigInterface $scopeConfig, array $data = [])
+    {
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context, $data);
+    }
+
+    private function getConfigValue($value)
+    {
+        return $this->scopeConfig->getValue("payment/Undostres_Gateway/" . $value, ScopeInterface::SCOPE_STORE);
+    }
 
     public function getTitle()
     {
-        return $this->getValue('payment/Undostres_Gateway/title');
+        return $this->getConfigValue("title");
     }
 
     public function isActive()
     {
-        return $this->getValue('payment/Undostres_Gateway/active');
+        return $this->getConfigValue("active");
     }
 
     public function getKey()
     {
-        return $this->getValue('payment/Undostres_Gateway/key');
+        return $this->getConfigValue("key");
     }
 
     public function getUrl()
     {
-        return $this->getValue('payment/Undostres_Gateway/url');
+        return $this->getConfigValue("url");
     }
 
     public function isLogging()
     {
-        return $this->getValue('payment/Undostres_Gateway/log');
+        return $this->getConfigValue("log");
     }
 }
